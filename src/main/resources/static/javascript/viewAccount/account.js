2609482920fa16397fd9115f9b447ad4a10febcc
Madison <madison.owens1221@gmail.com>
2017-09-05 23:27:00 -0500

    added database

--- src/main/resources/static/javascript/viewAccount/account.js
+++ src/main/resources/static/javascript/viewAccount/account.js
@@ -1,19 +1,35 @@
 angular.module('root.account', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/account', {
+  $routeProvider.when('/account/:userId', {
     templateUrl: '/javascript/viewAccount/account.html',
     controller: 'accountCtrl'
   });
 }])
 
-.controller('accountCtrl', ["$scope", function($scope) {
+.controller('accountCtrl', ["$scope", "$routeParams", function($scope, $routeParams) {
 
+//Write to database
+//firebase.database().ref('/user/' + $routeParams.userId).set({
+//    username: "paul"
+//  });
+
+//Read from database
+//firebase.database().ref('/user/' + $routeParams.userId).once('value').then(function(snapshot) {
+//    console.log((snapshot.val() && snapshot.val().username));
+//  });
+
+
+//get all users from database :D
+//manipulate json objects in javascript
+firebase.database().ref('/user/').once('value').then(function(snapshot) {
+    console.log((snapshot.val()));
+  });
+
+//Determine if user is logged in
     firebase.auth().onAuthStateChanged(function(user) {
         $scope.$apply(function(){
           if (user) {
-            console.log("user is logged in");
-                console.log(user.email);
             // User is signed in.
             var displayName = user.displayName;
             $scope.email = user.email;
@@ -24,11 +40,12 @@ angular.module('root.account', ['ngRoute'])
             var providerData = user.providerData;
             // ...
           } else {
-            console.log("user is logged out");
             // User is signed out.
             // ...
           }
         });
     });
+
+
 
 }]);
